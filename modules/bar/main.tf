f31e5321c582150d7c9f66dcc162f634f3f1763a
Ben Dean-Kawamura <ben@pculture.org>
2017-09-13 09:05:54 -0400

    Initial

---
+++ modules/bar/main.tf
@@ -0,0 +1,14 @@
+data "aws_region" "current" {
+  current = true
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags {
+    Name = "Test1"
+  }
+}
+
+output "aws_region" {
+  value = "${data.aws_region.current.name}"
+}
